eb9fc5909910beff3f022bebe2f4dff0bdee8868
rizkimahendra17 <riskimhndr170@gmail.com>
2022-04-06 17:09:53 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import CardView from '../views/CardView.vue'
+import SuccessView from '../views/SuccessView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+
+  {
+    path: '/card',
+    name: 'card',
+    component: CardView
+  },
+
+  {
+    path: '/success',
+    name: 'success',
+    component: SuccessView
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+  <!-- Ini component untuk menjalankan tampilan header -->
+  <TheHeader/>
+
+ <!-- Ini component untuk menjalankan tampilan Carousle -->
+  <TheCarousle/>
+
+  <!-- Ini component untuk menjalankan tampilan Hero Produk -->
+  <HeroWoman/>
+
+   <!-- Ini component untuk menjalankan tampilan Insta Gallery -->
+  <InstaGallery/>
+
+  <!-- Ini component untuk menjalankan tampilan Patner Logo -->
+  <PatnerLogo/>
+
+  <!-- Ini component untuk menjalankan tampilan The Footer -->
+  <TheFooter/>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+ import TheHeader from '@/components/TheHeader.vue';
+ import TheCarousle from '@/components/TheCarousle.vue';
+ import HeroWoman from '@/components/HeroWoman.vue';
+ import InstaGallery from '@/components/InstaGallery.vue';
+ import PatnerLogo from '@/components/PatnerLogo.vue';
+ import TheFooter from '@/components/TheFooter.vue';
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    TheHeader,
+    TheCarousle,
+    HeroWoman,
+    InstaGallery,
+    PatnerLogo,
+    TheFooter,
+    
+  }
+}
+</script>
